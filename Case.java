879caeaeafd82d83f201f81f4d54d870794a386b
tgindre <timothee.gindre@isep.fr>
2016-04-03 19:00:39 +0200

    Update case

--- Case.java
+++ Case.java
@@ -1,7 +1,17 @@
 
 public class Case {
 
+	int c;
+	int l;
+	char couleur;
+	
 	public static void main(String[] args) {
+	}
+	
+	public Case (int c, int l, char couleur){//coordonnées et couleur d'une case
+		this.c = c;
+		this.l = l;
+		this.couleur = couleur;
 	}
 
 }
