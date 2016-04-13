6d42a451e271088157c33b205a94b0ae6c0d7c2d
RomainCintas <romain.cintas1@gmail.com>
2016-04-13 11:14:45 +0200

    Espace

--- Main.java
+++ Main.java
@@ -46,29 +46,29 @@ public class Main {
 
 			for (int i1 = 0; i1<caseverif.size(); i1++ ){//défilement des cases adjacentes
 
-			
-
 				if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l][c+1]){//Vérification à droite
 					//On met en majuscule + changement de couleur
 					generation_grillage.grillage[l][c+1]=joueur1.couleur;
 					caseverif.add(new Case(c+1,l,couleur));
 					//Case case2 = new Case(c+1,l,couleur1);
 				}
-				if (joueur1.couleur == generation_grillage.grillage[l+1][c]){//Vérification en bas
+				if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l+1][c]){//Vérification en bas
 					//On met en majuscule + changement de couleur
 					generation_grillage.grillage[l+1][c]=joueur1.couleur;
 					caseverif.add(new Case(c,l+1,couleur));
 				}
-				if (joueur1.couleur == generation_grillage.grillage[l-1][c]){//Vérification en haut
+				if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l-1][c]){//Vérification en haut
 					//On met en majuscule + changement de couleur
 					generation_grillage.grillage[l-1][c]=joueur1.couleur;
 					caseverif.add(new Case(c,l-1,couleur));
 				}
-				if (joueur1.couleur == generation_grillage.grillage[l][c-1]){//Vérification à gauche
+				if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l][c-1]){//Vérification à gauche
 					//On met en majuscule + changement de couleur
 					generation_grillage.grillage[l][c-1]=joueur1.couleur;
 					caseverif.add(new Case(c-1,l,couleur));
 				}
+				joueur1.tour = 0;
+				joueur2.tour = 1;
 			}
 		}
 			
@@ -91,21 +91,23 @@ public class Main {
 							caseverif2.add(new Case(c+1,l,couleur));
 							//Case case2 = new Case(c+1,l,couleur1);
 						}
-						if (joueur1.couleur == generation_grillage.grillage[l+1][c]){//Vérification en bas
+						if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l+1][c]){//Vérification en bas
 							//On met en majuscule + changement de couleur
 							generation_grillage.grillage[l+1][c]=joueur1.couleur;
 							caseverif2.add(new Case(c,l+1,couleur));
 						}
-						if (joueur1.couleur == generation_grillage.grillage[l-1][c]){//Vérification en haut
+						if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l-1][c]){//Vérification en haut
 							//On met en majuscule + changement de couleur
 							generation_grillage.grillage[l-1][c]=joueur1.couleur;
 							caseverif2.add(new Case(c,l-1,couleur));
 						}
-						if (joueur1.couleur == generation_grillage.grillage[l][c-1]){//Vérification à gauche
+						if (joueur1.couleur.toLowerCase() == generation_grillage.grillage[l][c-1]){//Vérification à gauche
 							//On met en majuscule + changement de couleur
 							generation_grillage.grillage[l][c-1]=joueur1.couleur;
 							caseverif2.add(new Case(c-1,l,couleur));
 						}
+						joueur1.tour = 1;
+						joueur2.tour = 0;
 					}
 				}
 			}
